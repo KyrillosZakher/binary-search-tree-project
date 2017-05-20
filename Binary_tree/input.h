d89b1cc09038c5b567e77979497f35e2a91f4f33
KyrillosZakher <Kyrillos.botros96@eng-st.cu.edu.eg>
2017-05-20 17:05:09 +0200

    Update input.h

--- Binary_tree/input.h
+++ Binary_tree/input.h
@@ -1,8 +1,8 @@
 #ifndef INPUT_H_INCLUDED
 #define INPUT_H_INCLUDED
 #include <iostream>
-using namespace std;
 #include <string>
+using namespace std;
 string inName();
 string inDrugs();
 int inNum();
